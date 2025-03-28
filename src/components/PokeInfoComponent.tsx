7291ee3aac3553488327047faf3611746fddd8b6
Tvang93 <vangt39@gmail.com>
2025-03-28 14:19:12 -0700

    fixing more build errors

--- src/components/PokeInfoComponent.tsx
+++ src/components/PokeInfoComponent.tsx
@@ -38,7 +38,7 @@ const PokeInfoComponent = (props: PokeInfo1) => {
   useEffect(() => {
     if (GetFavoritesFromLocalStorage().includes(props.pokeName))
       setIsFavored(true);
-  });
+  }),[props.pokeName];
 
   useEffect(() => {
     setPokemonName(props.pokeName);
